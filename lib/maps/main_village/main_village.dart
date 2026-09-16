7873566d77e87d9d410b423009d83463e1e8f8c2
Lucas Velloso <lucas.velloso@dilettasolutions.com>
2026-09-16 08:58:53 -0300

    Adiciona casa e escola

--- lib/maps/main_village/main_village.dart
+++ lib/maps/main_village/main_village.dart
@@ -99,6 +99,8 @@ class _MainVillageMapState extends State<MainVillageMap> {
           ...tree(position: Vector2(tileSize * 51.5, tileSize * 29), type: TreeType.birch1),
           ...tree(position: Vector2(tileSize * 53, tileSize * 30), type: TreeType.pine1),
           ...tree(position: Vector2(tileSize * 44, tileSize * 20.25), type: TreeType.pine1),
+          ...tree(position: Vector2(tileSize * 45.5, tileSize * 37.5), type: TreeType.pine2),
+          ...tree(position: Vector2(tileSize * 51.5, tileSize * 37.5), type: TreeType.pine1),
           //bushes:
           ...bush(position: Vector2(tileSize * 0.5, tileSize * 4.25), type: BushType.bush2),
           ...bush(position: Vector2(tileSize * 17, tileSize * 1.75), type: BushType.bush2),
@@ -128,6 +130,9 @@ class _MainVillageMapState extends State<MainVillageMap> {
           ...bush(position: Vector2(tileSize * 49.5, tileSize * 30.5), type: BushType.bush2),
           ...bush(position: Vector2(tileSize * 56.75, tileSize * 20), type: BushType.bush1),
           ...bush(position: Vector2(tileSize * 58.5, tileSize * 16), type: BushType.bush2),
+          ...bush(position: Vector2(tileSize * 47, tileSize * 38.5), type: BushType.bush2),
+          ...bush(position: Vector2(tileSize * 50, tileSize * 38.5), type: BushType.bush1),
+          ...bush(position: Vector2(tileSize * 49.25, tileSize * 38.75), type: BushType.bush1),
           // Tree(position: Vector2(0, 0)),x
           Waterfall(position: Vector2(tileSize * 47, 0)),
           DayTimeClock(
